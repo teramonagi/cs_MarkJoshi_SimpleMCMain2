aa12f1dfed4b8da2ec02a72814bbd16536636f51
unknown <yana@.(none)>
2011-10-06 01:03:00 +0900

    First Commit

---
+++ SimpleMCMain2/PayOff1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace SimpleMCMain2
+{
+    public class PayOff
+    {
+        public enum OptionType { call, put };
+        public PayOff(double strike, OptionType type)
+        {
+            this.strike_ = strike;
+            this.type_ = type;        
+        }
+        public double Do(double spot)
+        {
+            double result = 0;
+            switch(type_)
+            {
+                case OptionType.call : 
+                    result = Math.Max(spot - strike_, 0);
+                    break;
+                case OptionType.put : 
+                    result = Math.Max(strike_ - spot, 0);
+                    break;
+                default:
+                    throw new Exception("Unknown Option Type");
+            }
+
+            return result;
+        }
+        
+        private double strike_;
+        private OptionType type_;
+    }
+}
---
+++ SimpleMCMain2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleMCMain2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //parameters
+            double expiry = 1.0;
+            double strike = 50.0;
+            double spot = 49.0;
+            double vol = 0.2;
+            double r = 0.01;
+            
+            //input number of montecarlo paths
+            ulong number_of_paths = 10000;
+            Console.Write("Enter number of montecarlo paths : ");
+            number_of_paths = ulong.Parse(Console.ReadLine());
+
+            //create payoff object
+            PayOff call_payoff = new PayOff(strike, PayOff.OptionType.call);
+            PayOff put_payoff  = new PayOff(strike, PayOff.OptionType.put);
+
+            //montecarlo simulation & output result
+            double result = SimpleMC.SimpleMonteCarlo2(call_payoff, expiry, spot, vol, r, number_of_paths);
+            Console.WriteLine("the call price is " + result.ToString());
+            
+            result = SimpleMC.SimpleMonteCarlo2(put_payoff, expiry, spot, vol, r, number_of_paths);
+            Console.WriteLine("the put price is " + result.ToString());
+
+        }
+    }
+}
